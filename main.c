69841b88f79d4fcd3f493865f0374ef03ed12372
EasyTrip24 <easytrip26@gmail.com>
2025-11-30 19:32:05 +0100

    finish v1

--- main.c
+++ main.c
@@ -5,16 +5,13 @@
 
 
 int main(){
-
     list l = get_grammaire_list("input/grammaire.txt");
-
     if (l != NULL) {
         printf("Grammar loaded successfully:\n");
         afficher_liste(l);
     } else {
         printf("Failed to load grammar\n");
     }
-
     printf("result : %d", check_string("bdddccccdcc", l));
 
     return 0;
